56c03b7337f4ce07ff8b07b4d1af5dae9b320607
Bogdan Condorachi <bogdan.condorachi@gmail.com>
2026-01-07 12:05:40 +0200

    refact: optimize visits view

--- src/panel/components/Views/VisitsView.vue
+++ src/panel/components/Views/VisitsView.vue
@@ -1,6 +1,7 @@
 <template>
+  <!-- TODO: Add more stats: Text stats (total unique visits total and per lang, most popular pages, etc) - Pie chart of page-by-page popularity - -->
   <k-grid variant="columns" style="column-gap: var(--spacing-8)">
-    <!-- TODO: Add more stats: Text stats (total unique visits total and per lang, most popular pages, etc) - Pie chart of page-by-page popularity - -->
+    <!-- Visits Over Time -->
     <k-column width="1/1">
       <k-simplestats-chart
         type="Bar"
@@ -16,13 +17,14 @@
       />
     </k-column>
 
+    <!-- Page Visits Over Time -->
     <k-column width="1/1">
       <k-simplestats-chart
         type="Line"
         height="450"
         download="Site_PageVisits.png"
         :label="$t('simplestats.visits.pagevisitsovertime')"
-        :chart-data="pageVisitsOverTimeDataSorted"
+        :chart-data="sortedPageVisitsOverTime"
         :chart-labels="charts.pageVisitsOverTime.labels"
         :x-title="$t('simplestats.charts.time')"
         :y-title="$t('simplestats.charts.visits')"
@@ -33,6 +35,7 @@
       />
     </k-column>
 
+    <!-- Languages Over Time -->
     <k-column width="2/3" v-if="languagesAreEnabled">
       <k-simplestats-chart
         type="Line"
@@ -50,6 +53,7 @@
       />
     </k-column>
 
+    <!-- Global Language Popularity -->
     <k-column width="1/3" v-if="languagesAreEnabled">
       <k-simplestats-chart
         type="Pie"
@@ -62,6 +66,7 @@
       />
     </k-column>
 
+    <!-- Page Stats Table -->
     <k-column>
       <k-simplestats-filter-table
         :label="$t('simplestats.visits.visitedpages')"
@@ -83,70 +88,57 @@ export default {
       charts: {
         visitsOverTime: {
           data: [],
-          labels: [],
+          labels: []
         },
         pageVisitsOverTime: {
           data: [],
-          labels: [],
+          labels: []
         },
         languagesOverTime: {
           data: [],
-          labels: [],
+          labels: []
         },
         globalLanguages: {
           data: [],
-          labels: [],
-        },
+          labels: []
+        }
       },
 
       table: {
         rows: [],
-        columns: {},
+        columns: {}
       },
 
-      languagesAreEnabled: false,
+      languagesAreEnabled: false
     };
   },
 
   computed: {
-    pageVisitsOverTimeDataSorted() {
+    sortedPageVisitsOverTime() {
       return [...this.charts.pageVisitsOverTime.data].sort((a, b) =>
         a.ss_uid < b.ss_uid ? -1 : a.ss_uid > b.ss_uid ? 1 : 0
       );
-    },
+    }
   },
 
   methods: {
     loadData(response) {
       const map = {
-        visitsOverTime: {
-          data: 'visitsovertimedata',
-          labels: 'chartperiodlabels',
-        },
-        pageVisitsOverTime: {
-          data: 'pagevisitsovertimedata',
-          labels: 'chartperiodlabels',
-        },
-        languagesOverTime: {
-          data: 'languagesovertimedata',
-          labels: 'chartperiodlabels',
-        },
-        globalLanguages: {
-          data: 'globallanguagesdata',
-          labels: 'chartlanguageslabels',
-        },
+        visitsOverTime: ['visitsovertimedata', 'chartperiodlabels'],
+        pageVisitsOverTime: ['pagevisitsovertimedata', 'chartperiodlabels'],
+        languagesOverTime: ['languagesovertimedata', 'chartperiodlabels'],
+        globalLanguages: ['globallanguagesdata', 'chartlanguageslabels']
       };
 
-      Object.entries(map).forEach(([key, fields]) => {
-        this.charts[key].data = response[fields.data] || [];
-        this.charts[key].labels = response[fields.labels] || [];
+      Object.entries(map).forEach(([key, [dataKey, labelKey]]) => {
+        this.charts[key].data = response[dataKey] || [];
+        this.charts[key].labels = response[labelKey] || [];
       });
 
       this.table.rows = response.pagestatsdata || [];
       this.table.columns = response.pagestatslabels || {};
-
-      this.languagesAreEnabled = Boolean(response.languagesAreEnabled);
-    },
-  },
+      this.languagesAreEnabled = !!response.languagesAreEnabled;
+    }
+  }
 };
 </script>
