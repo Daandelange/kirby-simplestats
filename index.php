1c5c1056b8de6a3bd8438a4974fc6f2f8f21fbd3
Bogdan Condorachi <bogdan.condorachi@gmail.com>
2026-01-08 20:03:40 +0200

    refact: move sections to config

--- index.php
+++ index.php
@@ -12,40 +12,8 @@ App::plugin('daandelange/simplestats', [
     'hooks'        => require 'src/config/hooks.php',
     'options'      => require 'src/config/options.php',
     'pageMethods'  => require 'src/config/pagemethods.php',
-    'routes'       => require 'src/config/routes.php',
-    'translations' => require 'src/config/i18n.php',
     'userMethods'  => require 'src/config/usermethods.php',
-
-    // One page stats detail section
-    'sections'  => [
-        'pagestats' => [
-            // Data that comes from blueprint
-            'props' => [
-                'headline' => function (string $headline = 'Page Stats') {
-                    return $headline;
-                },
-                'showFullInfo' => function (bool $showFullInfo = false) {
-                    return $showFullInfo;
-                },
-                'showTimeline' => function (bool $showTimeline = true) {
-                    return $showTimeline;
-                },
-                'showLanguages' => function (bool $showLanguages = true) {
-                    return $showLanguages;
-                },
-                'showTotals' => function (bool $showTotals = true) {
-                    return $showTotals;
-                },
-                'size'  => function(string $size = 'medium') {
-                    return $size;
-                },
-            ],
-            'computed' => [
-                'statsdata' => function () {
-                    return $this->model()->getPageStats();
-                }
-            ]
-        ],
-    ],
-
+    'routes'       => require 'src/config/routes.php',
+    'sections'     => require 'src/config/sections.php',
+    'translations' => require 'src/config/i18n.php',
 ]);
